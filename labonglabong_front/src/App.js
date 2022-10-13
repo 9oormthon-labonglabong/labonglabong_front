5a8e02fc38a0a853a1247330c0e5a1e41cadb8d2
Sunbeom <72551358+HwangSunBeom@users.noreply.github.com>
2022-10-13 11:08:28 +0900

    :sparkles: KakaoMap 출력
    카카오 지도를 출력합니다 :) #1

--- labonglabong_front/src/App.js
+++ labonglabong_front/src/App.js
@@ -1,16 +1,16 @@
-import { Alert, UncontrolledAlert } from "@goorm-dev/gds-goormthon";
-import { ActivityIcon, CheckCircleIcon } from "@goorm-dev/gds-goormthon";
-
 import { Route, Routes } from "react-router-dom";
 
 import Landing from "./pages/landing/Landing";
 import Calendar from "./components/Calendar";
+import Map from "./components/Map";
+import MapContainer from "./pages/map/MapContainer";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/calendar" element={<Calendar />} />
+      <Route path="/map" element={<MapContainer />} />
     </Routes>
   );
 }
